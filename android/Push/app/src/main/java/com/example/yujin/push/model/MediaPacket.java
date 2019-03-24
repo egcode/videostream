4b385b9b393a81447b713362b84f4cd6ed296874
Eugene G <you@example.com>
2019-03-24 14:37:25 -0700

    push android

---
+++ android/Push/app/src/main/java/com/example/yujin/push/model/MediaPacket.java
@@ -0,0 +1,31 @@
+package com.example.yujin.push.model;
+
+public class MediaPacket {
+
+    public enum Type {
+
+        VIDEO((byte) 1), AUDIO((byte) 0);
+
+        private byte type;
+
+        Type(byte type) {
+            this.type = type;
+        }
+
+        public byte getType() {
+            return type;
+        }
+
+        public static Type getType(byte value) {
+            for (Type type : Type.values()) {
+                if (type.getType() == value) {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public Type type;
+}
